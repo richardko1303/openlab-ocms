382f4bd12bb7f83ffd16882637c7d62bb3b509f0
richardko1303 <help.covidrobot@gmail.com>
2023-01-19 07:46:10 +0100

    Update ArrivalResource.php

--- plugins/openlab/arrivals/http/resources/ArrivalResource.php
+++ plugins/openlab/arrivals/http/resources/ArrivalResource.php
@@ -6,7 +6,11 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class ArrivalResource extends JsonResource {
     public function toArray($request) {
         return [
+            'id' => $this->id,
             'user' => $this->user,
+            'name' => $this->name,
+            'surname' => $this->surname,
+            'arrival' => $this->arrival,
         ];
     }
 }
